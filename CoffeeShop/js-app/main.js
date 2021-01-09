f64c6a23e3970791a09d1c0a49a9fa0488672b05
corneliajohnson <corneliaj91@gmail.com>
2021-01-08 19:52:38 -0500

    added text to dom

--- CoffeeShop/js-app/main.js
+++ CoffeeShop/js-app/main.js
@@ -1,12 +1,24 @@
 ﻿const url = "https://localhost:5001/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
+const beans = document.querySelector("#bean-varieties");
+let text = "";
+
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+          beanVarieties.map(bean =>
+              text += `
+            <div>
+                <h4>${bean.name}</h4>
+                <p>${bean.region}</p>
+                <p>${bean.notes}</p>
+            </div>`)
         })
+    beans.innerHTML = text;
 });
+
+
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
